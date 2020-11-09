df4ab6c169e2aea4dadd6b5c44caab3218e1c464
paulo4fs <engpaulofrota@gmail.com>
2020-11-09 14:52:29 -0300

    criada activity login e criado o seu layout

--- app/src/main/java/com/example/digitalhousefoods/SplashActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/SplashActivity.kt
@@ -1,11 +1,18 @@
 package com.example.digitalhousefoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        },500)
     }
 }
