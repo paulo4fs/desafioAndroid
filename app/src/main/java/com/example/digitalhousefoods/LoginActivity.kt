39715734f48f30587ec469aa383c5aafcca21eae
paulo4fs <engpaulofrota@gmail.com>
2020-11-09 16:17:38 -0300

    criado atividade singup

--- app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.example.digitalhousefoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        RegisterListener()
     }
-    
+    private fun RegisterListener() {
+        findViewById<MaterialButton>(R.id.btnRegisterLogin).setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/digitalhousefoods/SignupActivity.kt
@@ -0,0 +1,16 @@
+package com.example.digitalhousefoods
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+}
